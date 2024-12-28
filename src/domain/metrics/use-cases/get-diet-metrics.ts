7a45cc4bd0d5ecd8349360c548fa3e31295bc1b5
Dani Custodio <dani.ps.custodio@gmail.com>
2024-12-28 08:09:34 -0300

    ✨ feat(domain): add get metrics use case

---
+++ src/domain/metrics/use-cases/get-diet-metrics.ts
@@ -0,0 +1,75 @@
+import { type Either, right } from '@/core/either'
+import type { Meal } from '@/domain/meal/entities/meal'
+import type { MealRepository } from '@/domain/meal/repositories/meal-repository'
+
+interface Metrics {
+	insideDiet: number
+	outsideDiet: number
+	longestStreak: number
+	totalMeals: number
+}
+
+interface GetDietMetricsUseCaseRequest {
+	accountId: string
+}
+
+type GetDietMetricsUseCaseResponse = Either<Error, { metrics: Metrics }>
+
+export class GetDietMetricsUseCase {
+	constructor(private mealRepository: MealRepository) {}
+
+	async execute({
+		accountId,
+	}: GetDietMetricsUseCaseRequest): Promise<GetDietMetricsUseCaseResponse> {
+		const meals = await this.mealRepository.findAll(accountId)
+		const metrics = {
+			insideDiet: meals.filter((meal) => meal.isWithinTheDiet).length,
+			outsideDiet: meals.filter((meal) => !meal.isWithinTheDiet).length,
+			longestStreak: this.getLongestStreak(meals),
+			totalMeals: meals.length,
+		}
+
+		return right({ metrics })
+	}
+
+	private getLongestStreak(meals: Meal[]): number {
+		if (!meals?.length) return 0
+
+		const sortedMeals = [...meals].sort(
+			(a, b) => a.datetime.getTime() - b.datetime.getTime()
+		)
+
+		let longestStreak = 0
+		let currentStreak = 0
+		let previousDate: Date | null = null
+
+		for (const meal of sortedMeals) {
+			if (meal.isWithinTheDiet) {
+				if (
+					previousDate &&
+					this.isConsecutiveDay(meal.datetime, previousDate)
+				) {
+					currentStreak++
+				} else {
+					currentStreak = 1
+				}
+
+				longestStreak = Math.max(longestStreak, currentStreak)
+			} else {
+				currentStreak = 0
+			}
+
+			previousDate = meal.datetime
+		}
+
+		return longestStreak
+	}
+
+	private isConsecutiveDay(date: Date, previousDate: Date) {
+		return (
+			date.getDate() - previousDate.getDate() === 1 &&
+			date.getMonth() === previousDate.getMonth() &&
+			date.getFullYear() === previousDate.getFullYear()
+		)
+	}
+}
--- src/tests/factories/make-meal.ts
+++ src/tests/factories/make-meal.ts
@@ -7,7 +7,7 @@ export function makeMeal(overrides: Partial<MealProps> = {}): Meal {
 	return Meal.create({
 		name: faker.food.dish(),
 		description: faker.food.description(),
-		datetime: new Date(),
+		datetime: faker.date.between({ from: '2020-01-01', to: '2023-12-31' }),
 		isWithinTheDiet: false,
 		accountId: new UniqueEntityID(randomUUID()),
 		...overrides,
@@ -19,4 +19,23 @@ export function makeMeals(
 	count = 2
 ): Meal[] {
 	return Array.from({ length: count }, () => makeMeal(overrides))
+}
+
+export function makeMealsInAStreak(
+	overrides: Partial<MealProps> = {},
+	streak = 3,
+	initialDate = new Date(2023, 1, 1)
+): Meal[] {
+	const daysInMiliseconds = 24 * 60 * 60 * 1000
+	const meals: Meal[] = []
+	for (let i = 0; i < streak; i++) {
+		meals.push(
+			makeMeal({
+				...overrides,
+				datetime: new Date(initialDate.getTime() + i * daysInMiliseconds),
+			})
+		)
+	}
+
+	return meals
 }
