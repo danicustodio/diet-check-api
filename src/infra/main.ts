17f8f39f0e7c7f21bebced09e66385e1a5498956
Dani Custodio <dani.ps.custodio@gmail.com>
2025-03-03 15:28:12 -0300

    ✨ feat: add NestJS framework (#1)
    * 🔧 chore: add nestjs to the project
    
    * 📚 docs: add swagger

---
+++ src/infra/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Diet Check API')
+    .setDescription(
+      'Node.js-based app designed to help users track and manage their dietary habits by logging meals'
+    )
+    .setVersion('1.0')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentFactory)
+
+  await app.listen(process.env.PORT ?? 3000)
+}
+bootstrap()
