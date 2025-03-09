4be6a63e4f582051fa2cb3db957132caa4a96e55
Dani Custodio <dani.ps.custodio@gmail.com>
2025-03-09 15:25:04 -0300

    🔨 refactor(env): create an environment service for env var handling

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,11 @@
 import { AccountRepository } from '@/domain/account/repositories/account-repository'
 import { Module } from '@nestjs/common'
+import { EnvModule } from '../env/env.module'
 import { DrizzleService } from './drizzle/drizzle.service'
 import { DrizzleAccountRepository } from './drizzle/repositories/drizzle-account-repository'
 
 @Module({
+  imports: [EnvModule],
   providers: [
     DrizzleService,
     { provide: AccountRepository, useClass: DrizzleAccountRepository },
